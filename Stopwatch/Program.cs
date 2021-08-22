1e6eb07f7267916c5c5bcedb942e2dc41bb41422
Igor Cavichiolle <igorgabrielcavichiolle@hotmail.com>
2021-08-22 08:53:48 -0300

    22/08/2021 -Calculator, Stopwatch e TextEditor

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+
+            //data.Length - 1 (Pegando a posição do ultimo caractere da string)
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+            //Agora vamos da posição 0 até a ultima posição -1
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            //Multiplicador apenas ira mudar para 60 quando for escolhido a opção minutos
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            //Sempre passando o multiplicador
+            PreStart(time * multiplier);
+        }
+        static void PreStart(int pTime)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(pTime);
+        }
+        static void Start(int pTime)
+        {
+            int currentTime = 0;
+
+            while (currentTime != pTime)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
---
+++ TextEditor/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - abrir arquivo");
+            Console.WriteLine("2 - criar novo arquivo");
+            Console.WriteLine("0 - sair");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            //StreamReader recebe o caminho de um arquivo e abre o proprio
+            //ReadToEnd() le o arquivo até o final e armazena em text
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("------------------------");
+
+            string text = "";
+
+            //Utiliando o do para sempre realizar a ação e testar a condição depois de realizar
+            //Incrementa text com a linha digitada , enquanto a tecla digitada não foi selecionada ESC
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(text);
+
+        }
+
+        static void Salvar(string pText)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            //Criano um novo arquivo e salvando o conteudo de pText 
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(pText);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
