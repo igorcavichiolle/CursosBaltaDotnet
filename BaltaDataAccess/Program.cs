00a78d62ad22fcb293450ab4e5ad1f45aa789516
Igor Cavichiolle <igorgabrielcavichiolle@hotmail.com>
2021-08-27 10:13:17 -0300

    27/08/2021 - Utilizando ADO.NET Puro

---
+++ BaltaDataAccess/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    //Utilizando o ADO.NET Puro para conexão direta ao banco de dados
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+            // //Objeto de conexao com o banco de dados 
+            // var connection = new SqlConnection();
+
+            // //Abrindo conexao
+            // connection.Open();
+
+            // //Fechando conexao
+            // connection.Close();
+
+            // //Distroi o objeto SqlConnection (E Fecha a conexao)
+            // connection.Dispose();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado");
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+
+            }
+        }
+    }
+}
