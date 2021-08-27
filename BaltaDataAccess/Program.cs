9c2215864844d1f4dc4e6b56c49d09c1dbdbeab2
Igor Cavichiolle <igorgabrielcavichiolle@hotmail.com>
2021-08-27 10:17:13 -0300

    27/08/2021 - Utilizando ADO.NET Puro

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -8,7 +8,7 @@ namespace BaltaDataAccess
     {
         static void Main(string[] args)
         {
-            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=";
 
             // //Objeto de conexao com o banco de dados 
             // var connection = new SqlConnection();
